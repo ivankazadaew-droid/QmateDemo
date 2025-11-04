836e6d32cc6be9c05f39ad7a935340ceb3906c71
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2025-11-04 16:08:51 +0100

    Add types

--- test/utils/allure.utils.ts
+++ test/utils/allure.utils.ts
@@ -1,9 +1,9 @@
 import allure from '@wdio/allure-reporter'
 
 export async function takeScreenshotAndAttach() {
-    const screenshotBase64 = await browser.takeScreenshot();
+    const screenshotBase64: string = await browser.takeScreenshot();
 
-    const screenshotBuffer = Buffer.from(screenshotBase64, 'base64');
+    const screenshotBuffer: Buffer<ArrayBuffer> = Buffer.from(screenshotBase64, 'base64');
     
     allure.addAttachment('Screenshot', screenshotBuffer, 'image/png');
 }
