f3c04102e4ff195351f83ce0c961452c14f62d07
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2025-11-04 16:09:44 +0100

    Add missing types and imports

--- test/specs/homePage.spec.ts
+++ test/specs/homePage.spec.ts
@@ -2,30 +2,34 @@
 
 import { faker } from '@faker-js/faker';
 import allure from '@wdio/allure-reporter';
-import { takeScreenshotAndAttach } from '../utils/allure.utils.js'
+import { takeScreenshotAndAttach } from '../utils/allure.utils.ts'
 
-import HomePage from '../pageobjects/home.page.js';
-import CheckoutPage from '../pageobjects/checkout.page.js';
-import OrderSummary from '../pageobjects/orderSummary.page.js';
-import OrderCompletedPage from '../pageobjects/orderCompleted.page.js';
+import HomePage from '../pageobjects/home.page.ts';
+import CheckoutPage from '../pageobjects/checkout.page.ts';
+import OrderSummary from '../pageobjects/orderSummary.page.ts';
+import OrderCompletedPage from '../pageobjects/orderCompleted.page.ts';
+
+import { ProductCategory } from '../enums/productCategory.enum.ts';
+import { ProductFilter } from '../enums/productFilter.enum.ts';
+import { ProductStatus } from '../enums/productStatus.enum.ts'; 
 
 describe('Home Page tests', () => {
 
     it('Add any item', async () => {
 
-        const cardHolderName = faker.person.fullName();
-        let rawCardNumber = faker.finance.creditCardNumber(); 
-        const cardNumber = rawCardNumber.replace(/\D/g, '').substring(0, 16);
-        const securityCode = faker.finance.creditCardCVV(); 
-        const futureMonth = faker.date.future({ years: 5 }).getMonth() + 1;
-        const futureYear = faker.date.future({ years: 5 }).getFullYear();
-        const expirationDate = `${futureMonth.toString().padStart(2, '0')}/${futureYear}`; 
-        const streetName = faker.location.street(); 
-        const houseNumber = faker.string.numeric(1);
-        const address = `${streetName.split(' ')[0]} ${houseNumber}`;
-        const city = faker.location.city();
-        const zipCode = faker.location.zipCode('#####');
-        const country = faker.location.country();
+        const cardHolderName: string = faker.person.fullName();
+        let rawCardNumber: string  = faker.finance.creditCardNumber(); 
+        const cardNumber: string  = rawCardNumber.replace(/\D/g, '').substring(0, 16);
+        const securityCode: string  = faker.finance.creditCardCVV(); 
+        const futureMonth: number  = faker.date.future({ years: 5 }).getMonth() + 1;
+        const futureYear: number  = faker.date.future({ years: 5 }).getFullYear();
+        const expirationDate: string = `${futureMonth.toString().padStart(2, '0')}/${futureYear}`; 
+        const streetName: string  = faker.location.street(); 
+        const houseNumber: string  = faker.string.numeric(1);
+        const address: string  = `${streetName.split(' ')[0]} ${houseNumber}`;
+        const city: string  = faker.location.city();
+        const zipCode: string  = faker.location.zipCode('#####');
+        const country: string  = faker.location.country();
 
         await HomePage.open();
         await HomePage.waitForPageToLoad();
@@ -62,8 +66,8 @@ describe('Home Page tests', () => {
 
         await OrderCompletedPage.waitForPageToLoad();
 
-        const successMessageText = await OrderCompletedPage.getSuccessMessageText();
-        const expectedText = "Thank you for your order!";
+        const successMessageText: string = await OrderCompletedPage.getSuccessMessageText();
+        const expectedText: string = "Thank you for your order!";
 
         await common.assertion.expectToContain(successMessageText, expectedText);
     });
@@ -75,7 +79,7 @@ describe('Home Page tests', () => {
             await HomePage.waitForPageToLoad();
             await HomePage.clickAddToCartButtonForProductWithStatus(ProductStatus.OUT_OF_STOCK);
 
-            const isModalDisplayed = await HomePage.isOutOfStockConfirmationDialogDisplayed()
+            const isModalDisplayed: boolean = await HomePage.isOutOfStockConfirmationDialogDisplayed()
 
             await common.assertion.expectTrue(isModalDisplayed);
 
@@ -87,7 +91,7 @@ describe('Home Page tests', () => {
         await HomePage.open();
         await HomePage.waitForPageToLoad();
 
-        const expectedCategories = [
+        const expectedCategories: string[] = [
             "Accessories",
             "Computer System Accessories",
             "Desktop Computers",
@@ -106,18 +110,18 @@ describe('Home Page tests', () => {
             "Telecommunication",
         ];
 
-        const actualCategories = await HomePage.getAllCategoryTitles();
+        const actualCategories: string[] = await HomePage.getAllCategoryTitles();
 
         await common.assertion.expectEqual(actualCategories, expectedCategories); 
     });
 
-    it.skip('Verify product can be found via search bar', async () => {
+    it('Verify product can be found via search bar', async () => {
         await HomePage.open();
         await HomePage.waitForPageToLoad();
         
         await HomePage.searchForProduct("Screen clean");
         
-        const actualProducts = await HomePage.getAllProductTitles();
+        const actualProducts: string[] = await HomePage.getAllProductTitles();
 
         await common.assertion.expectEqual(actualProducts.length, 1);
         await common.assertion.expectEqual(actualProducts[0], "Screen clean");
@@ -133,7 +137,7 @@ describe('Home Page tests', () => {
         await HomePage.selectFilterOption(ProductStatus.AVAILABLE);
         await HomePage.applyFiltering();
 
-        let actualStatusesSet = new Set(await HomePage.getAllProductStatuses());
+        let actualStatusesSet: Set<string> = new Set<string>(await HomePage.getAllProductStatuses());
 
         await common.assertion.expectEqual(actualStatusesSet.size, 1);
         await common.assertion.expectTrue(actualStatusesSet.has(ProductStatus.AVAILABLE));
