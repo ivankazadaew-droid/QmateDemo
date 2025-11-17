2d135f2dc88770ba86544659b2cb5242b6f1caa4
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2025-11-17 12:21:02 +0100

    Update scenario with testDataGenerator

--- test/specs/homePage.spec.js
+++ test/specs/homePage.spec.js
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 import allure from '@wdio/allure-reporter';
 import { takeScreenshotAndAttach } from '../utils/allure.utils.js'
+import testDataGenerator from '../data/testDataGenerator.js';
 
 import HomePage from '../pageobjects/home.page.js';
 import CheckoutPage from '../pageobjects/checkout.page.js';
@@ -11,47 +12,32 @@ describe('Home Page tests', () => {
 
     it('Add any item', async () => {
 
-        const cardHolderName = faker.person.fullName();
-        let rawCardNumber = faker.finance.creditCardNumber(); 
-        const cardNumber = rawCardNumber.replace(/\D/g, '').substring(0, 16);
-        const securityCode = faker.finance.creditCardCVV(); 
-        const futureMonth = faker.date.future({ years: 5 }).getMonth() + 1;
-        const futureYear = faker.date.future({ years: 5 }).getFullYear();
-        const expirationDate = `${futureMonth.toString().padStart(2, '0')}/${futureYear}`; 
-        const streetName = faker.location.street(); 
-        const houseNumber = faker.string.numeric(1);
-        const address = `${streetName.split(' ')[0]} ${houseNumber}`;
-        const city = faker.location.city();
-        const zipCode = faker.location.zipCode('#####');
-        const country = faker.location.country();
+        const cardDetails = testDataGenerator.generateCardDetails();
+        const addressDetails = testDataGenerator.generateAddressDetails();
 
         await HomePage.open();
         await HomePage.waitForPageToLoad();
-
-        // Add item
 
         await HomePage.clickAddItemButton();
         await HomePage.clickCartButton();
         await HomePage.clickProceedCartButton();
 
-        // Checkout
-
         await CheckoutPage.waitForPageToLoad();
         await CheckoutPage.clickStep2Button();
         await CheckoutPage.clickStep3Button();
 
-        await CheckoutPage.inputCardHolderName(cardHolderName);
-        await CheckoutPage.inputCardNumber(cardNumber);
-        await CheckoutPage.inputSecurityCode(securityCode);
-        await CheckoutPage.inputExpirationDate(expirationDate);
+        await CheckoutPage.inputCardHolderName(cardDetails.cardHolderName);
+        await CheckoutPage.inputCardNumber(cardDetails.cardNumber);
+        await CheckoutPage.inputSecurityCode(cardDetails.securityCode);
+        await CheckoutPage.inputExpirationDate(cardDetails.expirationDate);
 
         await common.userInteraction.pressEnter();
 
         await CheckoutPage.clickStep4Button();
-        await CheckoutPage.inputAddress(address);
-        await CheckoutPage.inputCity(city);
-        await CheckoutPage.inputZipCode(zipCode);
-        await CheckoutPage.inputCountry(country);
+        await CheckoutPage.inputAddress(addressDetails.address);
+        await CheckoutPage.inputCity(addressDetails.city);
+        await CheckoutPage.inputZipCode(addressDetails.zipCode);
+        await CheckoutPage.inputCountry(addressDetails.country);
 
         await common.userInteraction.pressEnter();
 
@@ -61,8 +47,6 @@ describe('Home Page tests', () => {
         await OrderSummary.waitForPageToLoad();
         await OrderSummary.clickSubmitButton();
         await OrderSummary.clickConfirmationModalYesButton();
-
-        // Assertion
 
         await OrderCompletedPage.waitForPageToLoad();
 
