0f7d3e364f2d1c689f086cd30d0df90b5294bf71
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2025-11-17 12:16:39 +0100

    Update steps with testDataGenerator

--- test/steps/checkoutProcessSteps.js
+++ test/steps/checkoutProcessSteps.js
@@ -1,11 +1,11 @@
 import { Given, When, Then } from '@cucumber/cucumber';
-import { faker } from '@faker-js/faker';
 import allure from '@wdio/allure-reporter';
 
 import HomePage from '../pageobjects/home.page.js';
 import CheckoutPage from '../pageobjects/checkout.page.js';
 import OrderSummary from '../pageobjects/orderSummary.page.js';
 import OrderCompletedPage from '../pageobjects/orderCompleted.page.js';
+import testDataGenerator from '../data/testDataGenerator.js';
 
 Given(/^Home page is opened$|^Open Home page$/, async function () {
   await HomePage.open();
@@ -31,54 +31,43 @@ When('the user choses payment type', async function () {
 });
 
 When('the user fills in card details', async function () {
-  const cardHolderName = faker.person.fullName();
-  let rawCardNumber = faker.finance.creditCardNumber(); 
-  const cardNumber = rawCardNumber.replace(/\D/g, '').substring(0, 16);
-  const securityCode = faker.finance.creditCardCVV(); 
-  const futureMonth = faker.date.future({ years: 5 }).getMonth() + 1;
-  const futureYear = faker.date.future({ years: 5 }).getFullYear();
-  const expirationDate = `${futureMonth.toString().padStart(2, '0')}/${futureYear}`; 
+  const cardDetails = testDataGenerator.generateCardDetails();
 
   await CheckoutPage.clickStep3Button();
   await browser.takeScreenshot();
 
   await allure.step('Input cardholder name', async () => {
-    await CheckoutPage.inputCardHolderName(cardHolderName);
+    await CheckoutPage.inputCardHolderName(cardDetails.cardHolderName);
     await browser.takeScreenshot();
   });
   
   await allure.step('Input card number', async () => {
-    await CheckoutPage.inputCardNumber(cardNumber)
+    await CheckoutPage.inputCardNumber(cardDetails.cardNumber)
     await browser.takeScreenshot();
   });
   
   await allure.step('Input security code', async () => {
-    await CheckoutPage.inputSecurityCode(securityCode);
+    await CheckoutPage.inputSecurityCode(cardDetails.securityCode);
     await browser.takeScreenshot();
   });
 
   await allure.step('Input expiration date', async () => {
-    await CheckoutPage.inputExpirationDate(expirationDate);
+    await CheckoutPage.inputExpirationDate(cardDetails.expirationDate);
     await common.userInteraction.pressEnter();
     await browser.takeScreenshot();
   });
 });
 
 When('the user fills in invoice address details', async function () {
-  const streetName = faker.location.street(); 
-  const houseNumber = faker.string.numeric(1);
-  const address = `${streetName.split(' ')[0]} ${houseNumber}`;
-  const city = faker.location.city();
-  const zipCode = faker.location.zipCode('#####');
-  const country = faker.location.country();
+  const addressDetails = testDataGenerator.generateAddressDetails();
   
   await CheckoutPage.clickStep4Button();
   await browser.takeScreenshot();
 
-  await CheckoutPage.inputAddress(address);
-  await CheckoutPage.inputCity(city);
-  await CheckoutPage.inputZipCode(zipCode);
-  await CheckoutPage.inputCountry(country);
+  await CheckoutPage.inputAddress(addressDetails.address);
+  await CheckoutPage.inputCity(addressDetails.city);
+  await CheckoutPage.inputZipCode(addressDetails.zipCode);
+  await CheckoutPage.inputCountry(addressDetails.country);
   await common.userInteraction.pressEnter();
   await browser.takeScreenshot();
 });
